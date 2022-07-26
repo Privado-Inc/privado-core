5d40c3f72a8d685daea2ed41b2c6704dc2fc102e
Pandurang Patil <pandurang.patil@privado.ai>
2022-07-26 17:33:46 +0530

    parsing external rules and merging them
    Handling to parse external rules and merging them by removing
    duplicates.

--- src/main/scala/ai/privado/entrypoint/ScanProcessor.scala
+++ src/main/scala/ai/privado/entrypoint/ScanProcessor.scala
@@ -9,54 +9,97 @@ import io.joern.javasrc2cpg.{Config, JavaSrc2Cpg}
 import io.joern.x2cpg.X2Cpg.applyDefaultOverlays
 import io.shiftleft.codepropertygraph.generated.Languages
 import io.shiftleft.semanticcpg.language._
+import org.slf4j.LoggerFactory
 
 import java.util.UUID
+import scala.sys.exit
 import scala.util.{Failure, Success}
 
 object ScanProcessor extends CommandProcessor {
+  val logger = LoggerFactory.getLogger(this.getClass)
 
-  def processRules(): Rules = {
-    val ir: File = File(config.internalRulesPath.head)
-    val internalRules = ir.listRecursively
-      .filter(f => f.extension == Some(".yaml") || f.extension == Some(".YAML"))
-      .map(file => {
-        val filePath            = file.pathAsString
-        val fileName            = file.nameWithoutExtension
-        val immediateParentName = file.parent.nameWithoutExtension
-        parser.parse(file.contentAsString) match {
-          case Right(json) =>
-            import ai.privado.model.CirceEnDe._
-            json.as[Rules] match {
-              case Right(rules) =>
-                rules.copy(
-                  sources = rules.sources.map(x =>
-                    x.copy(
-                      filePath = filePath,
-                      fileName = fileName,
-                      parentName = immediateParentName,
-                      nodeType = NodeType.SOURCE.toString
+  def parseRules(rulesPath: String): Rules = {
+    val ir: File =
+      try File(rulesPath)
+      catch {
+        case ex: Throwable =>
+          logger.debug("File error: ", ex)
+          logger.error(f"Rules path '${rulesPath}' is not accessible")
+          exit(1)
+      }
+    val parsedRules =
+      try
+        ir.listRecursively
+          .filter(f => f.extension == Some(".yaml") || f.extension == Some(".YAML"))
+          .map(file => {
+            val filePath            = file.pathAsString
+            val fileName            = file.nameWithoutExtension
+            val immediateParentName = file.parent.nameWithoutExtension
+            parser.parse(file.contentAsString) match {
+              case Right(json) =>
+                import ai.privado.model.CirceEnDe._
+                json.as[Rules] match {
+                  case Right(rules) =>
+                    rules.copy(
+                      sources = rules.sources.map(x =>
+                        x.copy(
+                          filePath = filePath,
+                          fileName = fileName,
+                          parentName = immediateParentName,
+                          nodeType = NodeType.SOURCE.toString
+                        )
+                      ),
+                      sinks = rules.sinks.map(x =>
+                        x.copy(
+                          filePath = filePath,
+                          fileName = fileName,
+                          parentName = immediateParentName,
+                          nodeType = NodeType.withNameWithDefault(immediateParentName).toString
+                        )
+                      )
                     )
-                  ),
-                  sinks = rules.sinks.map(x =>
-                    x.copy(
-                      filePath = filePath,
-                      fileName = fileName,
-                      parentName = immediateParentName,
-                      nodeType = NodeType.withNameWithDefault(immediateParentName).toString
-                    )
-                  )
-                )
+                  case _ =>
+                    Rules(List[RuleInfo](), List[RuleInfo]())
+                }
               case _ =>
                 Rules(List[RuleInfo](), List[RuleInfo]())
             }
-          case _ =>
-            Rules(List[RuleInfo](), List[RuleInfo]())
-        }
-      })
-      .reduce((a, b) => a.copy(sources = a.sources ++ b.sources, sinks = a.sinks ++ b.sinks))
-    // TODO: remove this println
-    println(internalRules)
-    internalRules
+          })
+          .reduce((a, b) => a.copy(sources = a.sources ++ b.sources, sinks = a.sinks ++ b.sinks))
+      catch {
+        case ex: Throwable =>
+          logger.debug("File error: ", ex)
+          logger.error(f"Rules path '${rulesPath}' is not accessible")
+          exit(1)
+      }
+    parsedRules
+  }
+
+  def processRules(): Rules = {
+    var internalRules = Rules(List[RuleInfo](), List[RuleInfo]())
+    if (!config.ignoreInternalRules) {
+      internalRules = parseRules(config.internalRulesPath.head)
+    }
+    var externalRules = Rules(List[RuleInfo](), List[RuleInfo]())
+    if (!config.externalRulePath.isEmpty) {
+      externalRules = parseRules(config.externalRulePath.head)
+    }
+    /*
+     * NOTE: We want to override the external rules over internal in case of duplicates by id.
+     * While concatenating two lists (internal and external) elements from the first collection will be kept and
+     * elements from second collection will be discarded.
+     *
+     * e.g
+     * val sources     = externalRules.sources ++ internalRules.sources
+     * sources.distinctBy(_.id) - this will return unique list of elements duplicated by id.
+     * In case of duplicates it will keep the elements from "externalRules.sources".
+     * We don't know the internal logic. We came to this conclusion based on testing few samples.
+     */
+    val sources     = externalRules.sources ++ internalRules.sources
+    val sinks       = externalRules.sinks ++ internalRules.sinks
+    val mergedRules = Rules(sources.distinctBy(_.id), sinks.distinctBy(_.id))
+    logger.info(mergedRules.toString())
+    mergedRules
   }
   override def process(): Unit = {
     println("Hello Joern")
