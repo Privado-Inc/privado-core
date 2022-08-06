e84c6b2cb6fbd74e36748d36f859bc928a9ddf81
privconsult <privcon@privado.ai>
2022-08-06 16:10:23 +0200

    Boilerplate

---
+++ src/test/scala/ai/privado/passes/config/ConfigFilePassTest.scala
@@ -0,0 +1,42 @@
+package ai.privado.passes.config
+
+import better.files.File
+import io.shiftleft.codepropertygraph.generated.Cpg
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import io.shiftleft.semanticcpg.language._
+
+class ConfigFilePassTest extends AnyWordSpec with Matchers with BeforeAndAfterAll {
+
+  var cpg : Cpg = _
+
+  private val configFileContents = """
+      |server.port=8080
+      |spring.application.name=accounts
+      |server.servlet.context-path=/accounts
+      |""".stripMargin
+
+  "ConfigFilePass" should {
+    "create a file node for the property file" in {
+      cpg.file.size shouldBe 1
+    }
+  }
+
+  var inputDir : File = _
+
+  override def beforeAll(): Unit = {
+    inputDir = File.newTemporaryDirectory()
+    (inputDir / "test.properties").write(configFileContents)
+    (inputDir / "unrelated.file").write("foo")
+    cpg = Cpg.empty
+    new ConfigFilePass(cpg, inputDir.toString).createAndApply()
+    super.beforeAll()
+  }
+
+  override def afterAll(): Unit = {
+    inputDir.delete()
+    super.afterAll()
+  }
+
+}
