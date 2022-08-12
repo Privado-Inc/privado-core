6f56f78cd95fb5702a96818cb489b3a451bbd744
Pandurang Patil <pandurang.patil@privado.ai>
2022-08-12 23:38:10 +0530

    Integrated s3 presigned URL file upload
    Integrated s3 presigned URL file upload to resolve larger file size
    problem

--- src/main/scala/ai/privado/auth/AuthenticationHandler.scala
+++ src/main/scala/ai/privado/auth/AuthenticationHandler.scala
@@ -5,9 +5,9 @@ import ai.privado.utility.Utilities
 import io.circe.Json
 import org.slf4j.LoggerFactory
 
-import java.io.File
-import java.security.MessageDigest
-import java.nio.file.{Paths, Files}
+import java.io.{File, FileInputStream}
+import java.nio.file.{Files, Paths}
+import java.security.{DigestInputStream, MessageDigest}
 
 object AuthenticationHandler {
   /*
@@ -79,27 +79,70 @@ object AuthenticationHandler {
     }
   }
 
+  // Compute a hash of a file
+  // The output of this function should match the output of running "md5 -q <file>"
+  def computeHash(path: String): String = {
+    val buffer = new Array[Byte](8192)
+    val md5    = MessageDigest.getInstance("MD5")
+
+    val dis = new DigestInputStream(new FileInputStream(new File(path)), md5)
+    try { while (dis.read(buffer) != -1) {} }
+    finally { dis.close() }
+
+    md5.digest.map("%02x".format(_)).mkString
+  }
+
   def pushDataToCloud(repoPath: String): String = {
     var BASE_URL = "https://api.code.privado.ai/prod"
     if (!Environment.isProduction.getOrElse("False").toBoolean) {
       BASE_URL = "https://t.api.code.privado.ai/test"
     }
-    val file              = new File(s"$repoPath/.privado/privado.json")
-    val uploadURL: String = s"$BASE_URL/cli/api/file/${Environment.userHash.get}"
-    val accessKey: String = Utilities.getSHA256Hash(Environment.dockerAccessKey.get)
     try {
-      val response = requests.post(
-        uploadURL,
-        data = requests.MultiPart(requests.MultiItem("scanfile", file, file.getName)),
-        headers = Map("Authentication" -> s"$accessKey")
-      )
-      val json = ujson.read(response.text())
-      response.statusCode match {
+      val file                         = new File(s"$repoPath/.privado/privado.json")
+      val md5hash                      = computeHash(s"$repoPath/.privado/privado.json")
+      val accessKey: String            = Utilities.getSHA256Hash(Environment.dockerAccessKey.get)
+      val s3PresignGenEndpoint: String = s"$BASE_URL/cli/api/file/presigned/${Environment.userHash.get}/${md5hash}"
+      val firstResp = requests.get(url = s3PresignGenEndpoint, headers = Map("Authentication" -> s"$accessKey"))
+      firstResp.statusCode match {
         case 200 =>
-          s"""\n> Successfully synchronized results with Privado Cloud \n> Continue to view results on: ${json(
-              "redirectUrl"
-            ).toString()}\n"""
-        case _ => json("message").toString()
+          val fresp = ujson.read(firstResp.text())
+          val secResp = requests.post(
+            url = fresp("s3PreSignedUrl")("url").str,
+            data = requests.MultiPart(
+              requests.MultiItem("key", fresp("s3PreSignedUrl")("fields")("key").str),
+              requests.MultiItem("x-amz-algorithm", fresp("s3PreSignedUrl")("fields")("x-amz-algorithm").str),
+              requests.MultiItem("x-amz-credential", fresp("s3PreSignedUrl")("fields")("x-amz-credential").str),
+              requests.MultiItem("x-amz-date", fresp("s3PreSignedUrl")("fields")("x-amz-date").str),
+              requests.MultiItem("policy", fresp("s3PreSignedUrl")("fields")("policy").str),
+              requests.MultiItem("x-amz-signature", fresp("s3PreSignedUrl")("fields")("x-amz-signature").str),
+              requests.MultiItem("file", file)
+            )
+          )
+          secResp.statusCode match {
+            case 204 =>
+              val finalResp = requests.post(
+                url =
+                  s"$BASE_URL/cli/api/file/process/${Environment.userHash.get}?filePath=${fresp("s3PreSignedUrl")("fields")("key").str}",
+                headers = Map("Authentication" -> s"$accessKey")
+              )
+              val json = ujson.read(finalResp.text())
+              finalResp.statusCode match {
+                case 200 =>
+                  s"""\n> Successfully synchronized results with Privado Cloud \n> Continue to view results on: ${json(
+                      "redirectUrl"
+                    ).toString()}\n"""
+                case _ =>
+                  logger.debug("Error while upload file to server")
+                  "Error occurred while uploading the file to the cloud."
+              }
+            case _ =>
+              logger.debug("Error while upload file to server")
+              "Error occurred while uploading the file to the cloud."
+          }
+
+        case _ =>
+          logger.debug("Error while generating upload request")
+          "Error occurred while uploading the file to the cloud."
       }
 
     } catch {
