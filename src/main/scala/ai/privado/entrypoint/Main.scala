bdd68408762833a985856b57ae8e11dc8866f9b7
Khemraj Rathore <khemraj.rathore@privado.ai>
2022-08-20 18:08:07 +0530

    standalone cleanup

--- src/main/scala/ai/privado/entrypoint/Main.scala
+++ src/main/scala/ai/privado/entrypoint/Main.scala
@@ -25,11 +25,6 @@ package ai.privado.entrypoint
 import ai.privado.auth.AuthenticationHandler
 import ai.privado.entrypoint.ScanProcessor.config
 import ai.privado.metric.MetricHandler
-import io.shiftleft.codepropertygraph.generated.nodes.{NewCredentials, NewTag}
-import io.shiftleft.codepropertygraph.generated.{Cpg, EdgeTypes}
-import io.shiftleft.passes.SimpleCpgPass
-import io.shiftleft.semanticcpg.language._
-import overflowdb.BatchedUpdate
 import org.slf4j.LoggerFactory
 
 /** Privado Core main entry point
@@ -62,27 +57,5 @@ object Main {
       // arguments are bad, error message should get displayed from inside CommandParser.parse
     }
 
-  }
-}
-
-/** TODO: get rid of this class Example of a custom pass that creates and stores a node in the CPG.
-  */
-class MyPass(cpg: Cpg) extends SimpleCpgPass(cpg) {
-  override def run(builder: BatchedUpdate.DiffGraphBuilder): Unit = {
-    // val n = NewMynodetype().myproperty("foo")
-    // builder.addNode(n)
-    cpg.literal.foreach(ident => builder.addEdge(ident, NewTag().name("Name").value("Sensitive"), EdgeTypes.TAGGED_BY))
-  }
-}
-
-/** TODO: get rid of this class Example of a custom pass that creates and stores a node in the CPG.
-  */
-class MyCredPass(cpg: Cpg) extends SimpleCpgPass(cpg) {
-  override def run(builder: BatchedUpdate.DiffGraphBuilder): Unit = {
-    cpg.literal.code(".*password.*").foreach { literal =>
-      val credential = NewCredentials().code(literal.code)
-      builder.addNode(credential)
-      builder.addEdge(literal, credential, EdgeTypes.IS_CREDENTIAL)
-    }
   }
 }
