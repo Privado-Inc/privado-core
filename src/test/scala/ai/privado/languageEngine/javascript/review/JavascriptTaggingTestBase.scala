0cba062e7e572be5e1ea4f8a1b9a47a2b5a0bc83
Khemraj Rathore <khemraj.rathore@privado.ai>
2022-10-10 22:12:56 +0530

    pass review

---
+++ src/test/scala/ai/privado/languageEngine/javascript/review/JavascriptTaggingTestBase.scala
@@ -0,0 +1,65 @@
+/*
+ * This file is part of Privado OSS.
+ *
+ * Privado is an open source static code analysis tool to discover data flows in the code.
+ * Copyright (C) 2022 Privado, Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, contact support@privado.ai
+ *
+ */
+
+package ai.privado.languageEngine.javascript.review
+
+import ai.privado.cache.RuleCache
+import ai.privado.model.ConfigAndRules
+import better.files.File
+import io.joern.jssrc2cpg.{Config, JsSrc2Cpg}
+import io.shiftleft.codepropertygraph.generated.Cpg
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+abstract class JavascriptTaggingTestBase extends AnyWordSpec with Matchers with BeforeAndAfterAll {
+
+  var cpg: Cpg = _
+  val javascriptFileContents: String
+  val packageJsonFileContents: String
+  var inputDir: File   = _
+  var outputFile: File = _
+  val rule: ConfigAndRules
+
+  override def beforeAll(): Unit = {
+    inputDir = File.newTemporaryDirectory()
+    (inputDir / "generalFile.js").write(javascriptFileContents)
+    (inputDir / "unrelated.file").write("foo")
+    (inputDir / "package.json").write(packageJsonFileContents)
+    outputFile = File.newTemporaryFile()
+    val config = Config(inputPath = inputDir.toString(), outputPath = outputFile.toString())
+    cpg = new JsSrc2Cpg().createCpg(config).get
+
+    // Caching Rule
+    RuleCache.setRule(rule)
+    super.beforeAll()
+  }
+
+  override def afterAll(): Unit = {
+    inputDir.delete()
+    cpg.close()
+    outputFile.delete()
+    super.afterAll()
+  }
+
+}
