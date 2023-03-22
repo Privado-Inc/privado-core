6b2832c381c0f7aebca849b26709271a2f13007c
Gaurav Gogia <16029099+gaurav-gogia@users.noreply.github.com>
2023-03-22 20:22:30 +0530

    using argument filters instead of regex

--- src/main/scala/ai/privado/languageEngine/java/tagger/Utility/GRPCTaggerUtility.scala
+++ src/main/scala/ai/privado/languageEngine/java/tagger/Utility/GRPCTaggerUtility.scala
@@ -8,32 +8,53 @@ import ai.privado.model.{CatLevelOne, Language, NodeType, RuleInfo}
 import io.shiftleft.codepropertygraph.generated.nodes.{Call, Method}
 
 object GRPCTaggerUtility {
+
+  // StreamObserverPattern to be used to identify type of field access for onNext & onCompleted methods
+  private val StreamObserverPattern = "(?i)(.*)(StreamObserver)(.*)"
+
   def getGrpcEndpoints(cpg: Cpg): List[Method] = {
-
-    // `onCompleted` is always called inside of gRPC service methods
-    // This `onCompleted` always has a StreamObserver in signature and it doesn't take any arguments
-    val OnCompletedPattern = "(?i)(.*)(StreamObserver)(.*)(onCompleted)(.*)([(](0)[)])$"
-
-    // Detecting `onCompleted` call by traversing function calls inside of all methods to narrow down gRPC services
-    return cpg.method.where(_.call.methodFullName(OnCompletedPattern)).l
+// Detecting `onCompleted` call by traversing function calls inside of all methods to narrow down gRPC services
+    return cpg
+      .call("onCompleted")
+      .filter(_.argument.size == 1)
+      .where(_.argument.typ.fullName(StreamObserverPattern))
+      .method
+      .l
   }
 
   def getGrpcSinkCalls(cpg: Cpg, grpcEndpoints: List[Method]): ListBuffer[Call] = {
     // `onNext` is always called inside of gRPC service methods
     // This `onNext` always has a StreamObserver in signature and takes only one argument
-    val OnNextPattern = "(?i)(.*)(StreamObserver)(.*)(onNext)(.*)([(](1)[)])$"
+    val OnNext        = "onNext"
     var grpcSinkCalls = ListBuffer[Call]()
 
     grpcEndpoints.foreach(endpoint => {
       // Detecting gRPC API sink calls. These sink calls have the same name as function calls inside of onNext functions
       // To identify correct sinks, this filter makes sure sink call's parent is NOT `onNext`
-      val callList = cpg.call(endpoint.name).whereNot(_.astParent.isCall.methodFullName(OnNextPattern)).l
+      val callList = cpg
+        .call(endpoint.name)
+        .whereNot(
+          _.astParent.isCall
+            .name(OnNext)
+            .filter(_.argument.size == 1)
+            .where(_.argument.typ.fullName(StreamObserverPattern))
+        )
+        .l
 
       // Detecting `onNext` call inside of gRPC endpoint method
       // `onNext` takes on argument, this should be the call that satisfies gRPC/proto file contract
       // Get full type name of arguments that the server/endpoint will process
       val endpointArgTypes =
-        endpoint.call.methodFullName(OnNextPattern).argument.isCall.argument.isIdentifier.typeFullName.l
+        endpoint.call
+          .name(OnNext)
+          .filter(_.argument.size == 1)
+          .where(_.argument.typ.fullName(StreamObserverPattern))
+          .argument
+          .isCall
+          .argument
+          .isIdentifier
+          .typeFullName
+          .l
 
       callList.foreach(sinkCall => {
         // Get full type name of arguments going inside gRPC sink call
