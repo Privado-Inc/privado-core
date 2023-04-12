abb3d9a89f92b1646697e65b8d3bfe9a41d947b1
Abhinav <abhinav.raj@privado.ai>
2023-04-12 14:26:10 +0530

    add error handling for case of parsing failure

--- src/main/scala/ai/privado/utility/SQLParser.scala
+++ src/main/scala/ai/privado/utility/SQLParser.scala
@@ -1,22 +1,33 @@
 package ai.privado.utility
 
+import net.sf.jsqlparser.JSQLParserException
 import net.sf.jsqlparser.parser.CCJSqlParserUtil
 import net.sf.jsqlparser.schema.Table
 import net.sf.jsqlparser.statement.select.{PlainSelect, Select}
+import org.slf4j.{Logger, LoggerFactory}
 
 object SQLParser {
 
+  val logger: Logger = LoggerFactory.getLogger(getClass)
+
   def parseSQL(sqlQuery: String) = {
-    val statement = CCJSqlParserUtil.parse(sqlQuery)
-    if (statement.isInstanceOf[Select]) {
-      val select      = statement.asInstanceOf[Select]
-      val plainSelect = select.getSelectBody.asInstanceOf[PlainSelect]
-      val columns     = plainSelect.getSelectItems.toArray.map(_.toString)
-      val table       = plainSelect.getFromItem.asInstanceOf[Table].toString
-      (table, columns)
-    } else {
-      (None, Array(""))
+    try {
+      val statement = CCJSqlParserUtil.parse(sqlQuery)
+      if (statement.isInstanceOf[Select]) {
+        val select      = statement.asInstanceOf[Select]
+        val plainSelect = select.getSelectBody.asInstanceOf[PlainSelect]
+        val columns     = plainSelect.getSelectItems.toArray.map(_.toString)
+        val table       = plainSelect.getFromItem.asInstanceOf[Table].toString
+        (table, columns)
+      } else {
+        (None, Array(""))
+      }
+    } catch {
+      case ex: JSQLParserException =>
+        logger.warn("Failed to parse the SQL query ", sqlQuery)
+        (None, Array(""))
     }
+
   }
 
 }
