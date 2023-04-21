dac5ec464ba03b14647eaa0af9fc81fbf6778528
hiteshbedre <hitesh.bedre@privado.com>
2023-04-21 12:45:25 +0530

    refactored inherit tagger

--- src/main/scala/ai/privado/languageEngine/java/tagger/PrivadoTagger.scala
+++ src/main/scala/ai/privado/languageEngine/java/tagger/PrivadoTagger.scala
@@ -28,13 +28,12 @@ import ai.privado.entrypoint.{PrivadoInput, ScanProcessor, TimeMetric}
 import ai.privado.languageEngine.java.feeder.StorageInheritRule
 import ai.privado.languageEngine.java.passes.read.DatabaseReadPass
 import ai.privado.languageEngine.java.tagger.collection.{CollectionTagger, GrpcCollectionTagger, SOAPCollectionTagger}
-import ai.privado.languageEngine.java.tagger.sink.JavaAPITagger
+import ai.privado.languageEngine.java.tagger.sink.{InheritMethodTagger, JavaAPITagger}
 import ai.privado.languageEngine.java.tagger.source.{IdentifierTagger, InSensitiveCallTagger}
 import ai.privado.tagger.PrivadoBaseTagger
 import ai.privado.tagger.config.DBConfigTagger
 import ai.privado.tagger.sink.{CustomInheritTagger, RegularSinkTagger}
 import ai.privado.tagger.source.{LiteralTagger, SqlQueryTagger}
-
 import io.shiftleft.codepropertygraph.generated.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes.Tag
 import io.shiftleft.semanticcpg.language._
@@ -97,9 +96,8 @@ class PrivadoTagger(cpg: Cpg) extends PrivadoBaseTagger {
     if (!ScanProcessor.config.ignoreInternalRules) {
       // Adding custom rule to cache
       StorageInheritRule.rules.foreach(ruleCache.setRuleInfo)
-      StorageInheritRule.rules.foreach(ruleCache.addStorageRuleInfo)
       println(s"${Calendar.getInstance().getTime} - --CustomInheritTagger invoked...")
-      new CustomInheritTagger(cpg, ruleCache).createAndApply()
+      new InheritMethodTagger(cpg, ruleCache).createAndApply()
       println(
         s"${TimeMetric.getNewTime()} - --CustomInheritTagger is done in \t\t- ${TimeMetric.setNewTimeToStageLastAndGetTimeDiff()}"
       )
