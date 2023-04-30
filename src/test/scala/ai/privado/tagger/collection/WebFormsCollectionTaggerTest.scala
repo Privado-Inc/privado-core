be48c81e1319547c85357a7a102b38e2de4053a9
Ankit Kumar <ankit.kumar@privado.ai>
2023-05-01 04:42:14 +0530

    code refactoring

--- src/test/scala/ai/privado/tagger/collection/WebFormsCollectionTaggerTest.scala
+++ src/test/scala/ai/privado/tagger/collection/WebFormsCollectionTaggerTest.scala
@@ -220,7 +220,20 @@ class WebFormsCollectionTaggerTest extends AnyWordSpec with Matchers with Before
     val outputFile = File.newTemporaryFile()
     outPutFiles.addOne(outputFile)
     val rule: ConfigAndRules =
-      ConfigAndRules(sourceRule, List(), collectionRule, List(), List(), List(), List(), List(), List(), List(), List(), List())
+      ConfigAndRules(
+        sourceRule,
+        List(),
+        collectionRule,
+        List(),
+        List(),
+        List(),
+        List(),
+        List(),
+        List(),
+        List(),
+        List(),
+        List()
+      )
     val ruleCache = new RuleCache()
     ruleCache.setRule(rule)
     val config = Config(inputPath = inputDir.toString(), outputPath = outputFile.toString())
@@ -239,7 +252,20 @@ class WebFormsCollectionTaggerTest extends AnyWordSpec with Matchers with Before
     val outputFile = File.newTemporaryFile()
     outPutFiles.addOne(outputFile)
     val rule: ConfigAndRules =
-      ConfigAndRules(sourceRule, List(), collectionRule, List(), List(), List(), List(), List(), List(), List(), List(), List())
+      ConfigAndRules(
+        sourceRule,
+        List(),
+        collectionRule,
+        List(),
+        List(),
+        List(),
+        List(),
+        List(),
+        List(),
+        List(),
+        List(),
+        List()
+      )
     val ruleCache = new RuleCache()
     ruleCache.setRule(rule)
     val config = Config(inputPath = inputDir.toString(), outputPath = outputFile.toString())
