fcd619a0a85bb3591ccad1db25e1691ded15671e
Khemraj Rathore <khemraj.rathore@privado.ai>
2023-05-16 12:00:35 +0530

    support for named argument

--- src/main/scala/ai/privado/languageEngine/python/semantic/PythonSemanticGenerator.scala
+++ src/main/scala/ai/privado/languageEngine/python/semantic/PythonSemanticGenerator.scala
@@ -1,12 +1,16 @@
 package ai.privado.languageEngine.python.semantic
 
 import ai.privado.cache.RuleCache
-import ai.privado.model.{CatLevelOne, Constants}
+import ai.privado.model.Language.UNKNOWN
+import ai.privado.model.{CatLevelOne, Constants, Semantic}
 import ai.privado.semantic.SemanticGenerator
 import io.joern.dataflowengineoss.semanticsloader.{Parser, Semantics}
 import io.shiftleft.codepropertygraph.generated.Cpg
+import io.shiftleft.codepropertygraph.generated.nodes.Call
 import io.shiftleft.semanticcpg.language._
 import org.slf4j.LoggerFactory
+
+import scala.collection.mutable
 
 object PythonSemanticGenerator extends SemanticGenerator {
 
@@ -16,8 +20,7 @@ object PythonSemanticGenerator extends SemanticGenerator {
     val customSinkSemantics = getMaximumFlowSemantic(
       cpg.call
         .where(_.tag.nameExact(Constants.catLevelOne).valueExact(CatLevelOne.SINKS.name))
-        .where(_.tag.nameExact(Constants.catLevelTwo).valueExact(Constants.leakages))
-        .map(generateSemanticForTaint(_, -1, extraParameter = 1))
+        .map(generateSemanticForTaint(_, -1))
     )
 
     val semanticFromConfig = ruleCache.getRule.semantics.flatMap(generateSemantic).sorted
@@ -31,6 +34,24 @@ object PythonSemanticGenerator extends SemanticGenerator {
     val parsed         = new Parser().parse(list.mkString("\n"))
     val finalSemantics = PythonSemanticGenerator.getDefaultSemantics.elements ++ parsed
     Semantics.fromList(finalSemantics)
+  }
+
+  def generateSemanticForTaint(callNode: Call, toTaint: Int): Semantic = {
+    val argumentList = callNode.argument.flatMap { arg =>
+      if (arg.argumentIndex != -1)
+        Some(arg.argumentIndex)
+      else if (arg.argumentName.isDefined)
+        Some("\"" + arg.argumentName.get + "\"")
+      else
+        None
+    }.l
+    val parameterSemantic = mutable.HashSet[String]()
+    argumentList.map { item =>
+      if (toTaint != -2)
+        parameterSemantic.add(s"$item->$toTaint")
+      parameterSemantic.add(s"$item->$item")
+    }
+    Semantic(callNode.methodFullName, parameterSemantic.toList.sorted.mkString(" ").trim, "", UNKNOWN, Array())
   }
 
 }
