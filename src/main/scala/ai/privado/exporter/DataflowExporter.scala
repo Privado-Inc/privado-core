a0bccb2c74200281bde44a5665386c10efc310ac
hiteshbedre <32206192+hiteshbedre@users.noreply.github.com>
2023-07-05 20:39:29 +0530

    code formatting

--- src/main/scala/ai/privado/exporter/DataflowExporter.scala
+++ src/main/scala/ai/privado/exporter/DataflowExporter.scala
@@ -113,7 +113,9 @@ class DataflowExporter(cpg: Cpg, dataflowsMap: Map[String, Path], taggerCache: T
         sinkMap(sinkId) = ListBuffer()
       sinkMap(sinkId).append(sourceModel.pathId)
     })
-    sinkMap.map(entrySet => convertSink(sourceId, entrySet._1, entrySet._2.toSet.toList, sinkApiUrls(entrySet._1))).toList
+    sinkMap
+      .map(entrySet => convertSink(sourceId, entrySet._1, entrySet._2.toSet.toList, sinkApiUrls(entrySet._1)))
+      .toList
 
   }
 
