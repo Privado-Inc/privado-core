42bcce06ba7fc0235aa5c2753d9b42c06ec7b9ec
Karan Batavia <karan.batavia@privado.ai>
2023-07-19 16:08:10 +0530

    import nodes not to be considered as a resolved call node in report

--- src/main/scala/ai/privado/utility/UnresolvedReportUtility.scala
+++ src/main/scala/ai/privado/utility/UnresolvedReportUtility.scala
@@ -29,9 +29,10 @@ import ai.privado.model.Constants
 import ai.privado.model.Language
 
 import scala.collection.mutable.ListBuffer
-import io.shiftleft.semanticcpg.language._
+import io.shiftleft.semanticcpg.language.*
 import io.shiftleft.codepropertygraph.generated.Cpg
 import ai.privado.utility.Utilities.{getFileNameForNode, resolver}
+import io.shiftleft.codepropertygraph.generated.nodes.Call
 
 object UnresolvedReportUtility {
   def reportUnresolvedMethods(xtocpg: Try[Cpg], statoutdir: String, language: Language.Language): Unit = {
@@ -43,26 +44,43 @@ object UnresolvedReportUtility {
     var unresolvedNamespacesList      = ListBuffer[String]()
     var nonempty                      = 0
     var isempty                       = 0
+    var resolvedCalls                 = 0
 
     val unresolved_signature = "(?i)(.*)(unresolved)(signature)(.*)"
     val unresolved_namespace = "(?i)(.*)(unresolved)(namespace)(.*)"
-    val unknown_full_name    = "(?i)(.*)(unknownfullname)(.*)"
+    var unknown_full_name    = "(?i)(.*)(unknownfullname)(.*)"
     val operator_name        = "(?i)(<operator>).*"
+    val importRegex          = "(?i).*(import|require).*"
+
+    if (language.equals(Language.RUBY)) {
+      unknown_full_name = "(?i)(.*)(unknownfullname|empty)(.*)"
+    }
 
     var unresolved_sig_pattern = unknown_full_name
     if (language.equals(Language.JAVA)) {
       unresolved_sig_pattern = unresolved_signature
     }
 
+    val resolvedCallMethodFullNames = ListBuffer[Call]()
     xtocpg match {
       case Success(cpg) => {
-        val importCount   = cpg.call.l.filter((i) => i.name == "import").l.length
+        val importCount = cpg.call.l
+          .filter((i) => {
+            i.name.matches(importRegex)
+          })
+          .l
+          .length
         val operatorCount = cpg.call.l.filter(i => !i.name.matches(operator_name)).l.length
         total = cpg.call.methodFullName.l.length - importCount - operatorCount
-        unresolvedSignatures = cpg.call.methodFullName(unresolved_sig_pattern).l.length - importCount
+        unresolvedSignatures = cpg.call.methodFullName(unresolved_sig_pattern).l.length
 
         nonempty = cpg.call.callee.filter(_.nonEmpty == false).l.length
         isempty = cpg.call.callee.filter(_.isEmpty).l.length
+
+        // Resolved calls list
+        resolvedCalls = resolvedCallMethodFullNames
+          .addAll(cpg.call.whereNot(_.methodFullName(s"$unknown_full_name|$operator_name|$importRegex")).l)
+          .length
 
         cpg.call
           .methodFullName(unresolved_sig_pattern)
@@ -144,9 +162,6 @@ object UnresolvedReportUtility {
       statstr += s"$percentage% calls resolved\n"
     }
 
-    print(statstr)
-    statfile.appendText(statstr)
-
 //    if (nonempty > 0)
     statstr += s"\nCalls with nonEmpty Callee false: $nonempty"
 //    if (isempty > 0)
@@ -163,6 +178,20 @@ object UnresolvedReportUtility {
       statfile.appendLine("List of Calls with Unresolved Namespaces:")
       unresolvedNamespacesList.zipWithIndex.map { case (un, index) => statfile.appendLine(s"${(index + 1)} - $un") }
     }
+
+    statstr += s"$divider\n"
+    statstr += "Resolved calls: \n"
+
+    statfile.appendLine(divider)
+    statfile.appendLine("Resolved calls: \n")
+
+    resolvedCallMethodFullNames.foreach(call => {
+      statfile.appendLine(call.methodFullName)
+      statstr += s"${call.methodFullName}\n"
+    })
+
+    print(statstr)
+    statfile.appendText(statstr)
 
     println(divider)
     println()
