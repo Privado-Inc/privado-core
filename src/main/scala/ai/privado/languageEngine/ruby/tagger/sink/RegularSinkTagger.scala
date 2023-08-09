82f8dc83eec4adf080d5e8312d2f40d570b44bdf
Ankit Kumar <ankit.kumar@privado.ai>
2023-08-09 06:33:46 +0530

    Added dynamicHintType for sink tagging

--- src/main/scala/ai/privado/languageEngine/ruby/tagger/sink/RegularSinkTagger.scala
+++ src/main/scala/ai/privado/languageEngine/ruby/tagger/sink/RegularSinkTagger.scala
@@ -45,7 +45,10 @@ class RegularSinkTagger(cpg: Cpg, ruleCache: RuleCache) extends PrivadoParallelC
   }
 
   override def runOnPart(builder: DiffGraphBuilder, ruleInfo: RuleInfo): Unit = {
-    val sinks = cacheCall.methodFullName("(pkg.){0,1}(" + ruleInfo.combinedRulePattern + ").*").l
+    val combinedRegex = s"(pkg.){0,1}(\"${ruleInfo.combinedRulePattern}\").*"
+    val sinks = cacheCall
+      .or(_.methodFullName(combinedRegex), _.filter(_.dynamicTypeHintFullName.exists(_.matches(combinedRegex))))
+      .l
 
     sinks.foreach(sink => addRuleTags(builder, sink, ruleInfo, ruleCache))
   }
