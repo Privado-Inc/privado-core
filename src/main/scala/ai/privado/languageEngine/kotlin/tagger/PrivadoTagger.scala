53a60048364182f6f325c82674368f26bd6bcf09
Khemraj Rathore <khemraj.rathore@privado.ai>
2023-09-29 17:05:48 +0530

    support for api

--- src/main/scala/ai/privado/languageEngine/kotlin/tagger/PrivadoTagger.scala
+++ src/main/scala/ai/privado/languageEngine/kotlin/tagger/PrivadoTagger.scala
@@ -4,7 +4,7 @@ import ai.privado.cache.{RuleCache, TaggerCache}
 import ai.privado.entrypoint.PrivadoInput
 import ai.privado.languageEngine.kotlin.tagger.source.IdentifierTagger
 import ai.privado.tagger.PrivadoBaseTagger
-import ai.privado.tagger.sink.RegularSinkTagger
+import ai.privado.tagger.sink.{APITagger, RegularSinkTagger}
 import ai.privado.tagger.source.LiteralTagger
 import io.shiftleft.codepropertygraph.generated.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes.Tag
@@ -26,6 +26,8 @@ class PrivadoTagger(cpg: Cpg) extends PrivadoBaseTagger {
     new IdentifierTagger(cpg, ruleCache, taggerCache).createAndApply()
 
     new RegularSinkTagger(cpg, ruleCache).createAndApply()
+
+    new APITagger(cpg, ruleCache, privadoInputConfig).createAndApply()
 
     logger.info("Done with tagging")
     cpg.tag
