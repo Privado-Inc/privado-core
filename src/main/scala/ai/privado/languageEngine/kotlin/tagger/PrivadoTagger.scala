127376b7b8e5242d515bd43bde0763b40f3e7612
Khemraj Rathore <khemraj.rathore@privado.ai>
2023-10-03 16:14:10 +0530

    use same tagger as java

--- src/main/scala/ai/privado/languageEngine/kotlin/tagger/PrivadoTagger.scala
+++ src/main/scala/ai/privado/languageEngine/kotlin/tagger/PrivadoTagger.scala
@@ -2,10 +2,12 @@ package ai.privado.languageEngine.kotlin.tagger
 
 import ai.privado.cache.{RuleCache, TaggerCache}
 import ai.privado.entrypoint.PrivadoInput
-import ai.privado.languageEngine.kotlin.tagger.source.IdentifierTagger
+import ai.privado.languageEngine.java.tagger.config.JavaDBConfigTagger
+import ai.privado.languageEngine.java.tagger.sink.JavaAPITagger
+import ai.privado.languageEngine.java.tagger.source.{IdentifierTagger, InSensitiveCallTagger}
 import ai.privado.tagger.PrivadoBaseTagger
 import ai.privado.tagger.sink.{APITagger, RegularSinkTagger}
-import ai.privado.tagger.source.LiteralTagger
+import ai.privado.tagger.source.{LiteralTagger, SqlQueryTagger}
 import io.shiftleft.codepropertygraph.generated.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes.Tag
 import io.shiftleft.semanticcpg.language.*
@@ -23,7 +25,14 @@ class PrivadoTagger(cpg: Cpg) extends PrivadoBaseTagger {
     logger.info("Starting tagging")
 
     new LiteralTagger(cpg, ruleCache).createAndApply()
+
+    new SqlQueryTagger(cpg, ruleCache).createAndApply()
+
     new IdentifierTagger(cpg, ruleCache, taggerCache).createAndApply()
+
+    new InSensitiveCallTagger(cpg, ruleCache, taggerCache).createAndApply()
+
+    new JavaDBConfigTagger(cpg).createAndApply()
 
     new RegularSinkTagger(cpg, ruleCache).createAndApply()
 
