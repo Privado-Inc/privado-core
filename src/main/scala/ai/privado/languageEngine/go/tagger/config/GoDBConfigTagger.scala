2679daa935d8e3344f5486e8f8af9b6423bc0139
hiteshbedre <hitesh.bedre@privado.com>
2023-10-06 11:28:08 +0530

    go: property tagger

---
+++ src/main/scala/ai/privado/languageEngine/go/tagger/config/GoDBConfigTagger.scala
@@ -0,0 +1,32 @@
+package ai.privado.languageEngine.go.tagger.config
+
+import ai.privado.languageEngine.java.language.NodeStarters
+import ai.privado.tagger.PrivadoDBConfigBaseTagger
+import io.shiftleft.codepropertygraph.generated.Cpg
+import org.slf4j.LoggerFactory
+import overflowdb.BatchedUpdate
+import overflowdb.traversal.*
+
+class GoDBConfigTagger(cpg: Cpg) extends PrivadoDBConfigBaseTagger(cpg) {
+
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  override def run(builder: BatchedUpdate.DiffGraphBuilder): Unit = {
+    cpg.property.dedup.toArray
+      .filter(prop => prop.name.nonEmpty && prop.value.nonEmpty)
+      .foreach(dbUrl => {
+        try {
+          if (dbUrl.value.contains("dynamodb")) {
+            parsePropForDynamoDB(dbUrl)
+          } else if (dbUrl.value.contains("postgres:")) {
+            parsePropForPostgreSQL(dbUrl)
+          } else if (dbUrl.value.contains("mongo")) {
+            parsePropForMongoDB(dbUrl)
+          }
+        } catch {
+          case e: Exception => logger.debug("Exception while processing db config: " + e)
+        }
+      })
+  }
+
+}
