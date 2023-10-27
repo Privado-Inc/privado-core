34abd09b19086194339ffae0278840bcc0cd4b70
David Baker Effendi <dbe@sun.ac.za>
2023-10-27 13:47:50 +0200

    Re-introduced Cross-Closure DDG Edges via Pass Adding `ExperimentalLambdaDataFlowSupportPass` to add the `REACHING_DEF` edges back in that were reverted in Joern.

--- src/main/scala/ai/privado/languageEngine/javascript/processor/JavascriptProcessor.scala
+++ src/main/scala/ai/privado/languageEngine/javascript/processor/JavascriptProcessor.scala
@@ -24,7 +24,7 @@
 package ai.privado.languageEngine.javascript.processor
 
 import ai.privado.audit.AuditReportEntryPoint
-import ai.privado.cache.{AppCache, AuditCache, DataFlowCache, RuleCache, TaggerCache}
+import ai.privado.cache.*
 import ai.privado.entrypoint.ScanProcessor.config
 import ai.privado.entrypoint.{ScanProcessor, TimeMetric}
 import ai.privado.exporter.{ExcelExporter, JSONExporter}
@@ -33,17 +33,17 @@ import ai.privado.languageEngine.javascript.semantic.Language.*
 import ai.privado.metric.MetricHandler
 import ai.privado.model.Constants.*
 import ai.privado.model.{CatLevelOne, Constants, Language}
-import ai.privado.passes.{DBTParserPass, HTMLParserPass, SQLParser}
+import ai.privado.passes.{DBTParserPass, ExperimentalLambdaDataFlowSupportPass, HTMLParserPass, SQLParser}
 import ai.privado.semantic.Language.*
-import ai.privado.utility.{PropertyParserPass, UnresolvedReportUtility}
 import ai.privado.utility.Utilities.createCpgFolder
-import io.joern.jssrc2cpg.{Config, JsSrc2Cpg}
-import io.shiftleft.codepropertygraph
-import org.slf4j.LoggerFactory
-import io.shiftleft.semanticcpg.language.*
+import ai.privado.utility.{PropertyParserPass, UnresolvedReportUtility}
 import better.files.File
+import io.joern.jssrc2cpg.{Config, JsSrc2Cpg}
 import io.joern.x2cpg.passes.callgraph.NaiveCallLinker
+import io.shiftleft.codepropertygraph
 import io.shiftleft.codepropertygraph.generated.Operators
+import io.shiftleft.semanticcpg.language.*
+import org.slf4j.LoggerFactory
 
 import java.util.Calendar
 import scala.collection.mutable.ListBuffer
@@ -210,7 +210,9 @@ object JavascriptProcessor {
     val absoluteSourceLocation = File(sourceRepoLocation).path.toAbsolutePath.normalize().toString
     val cpgconfig =
       Config().withInputPath(absoluteSourceLocation).withOutputPath(cpgOutputPath)
-    val xtocpg = new JsSrc2Cpg().createCpgWithAllOverlays(cpgconfig)
+    val xtocpg = new JsSrc2Cpg()
+      .createCpgWithAllOverlays(cpgconfig)
+      .map(cpg => new ExperimentalLambdaDataFlowSupportPass(cpg).createAndApply())
     processCPG(xtocpg, ruleCache, sourceRepoLocation, dataFlowCache, auditCache)
   }
 
---
+++ src/main/scala/ai/privado/passes/ExperimentalLambdaDataFlowSupportPass.scala
@@ -0,0 +1,108 @@
+package ai.privado.passes
+
+import io.joern.dataflowengineoss.passes.reachingdef.*
+import io.joern.dataflowengineoss.semanticsloader.Semantics
+import io.joern.dataflowengineoss.{globalFromLiteral, identifierToFirstUsages}
+import io.shiftleft.codepropertygraph.generated.nodes.*
+import io.shiftleft.codepropertygraph.generated.{Cpg, EdgeTypes, PropertyNames}
+import io.shiftleft.passes.ForkJoinParallelCpgPass
+import io.shiftleft.semanticcpg.language.*
+import org.slf4j.LoggerFactory
+
+import scala.collection.mutable
+
+/**
+ * Enables DDG edges from variables outside of a closure to their references inside.
+ */
+class ExperimentalLambdaDataFlowSupportPass(cpg: Cpg, maxNumberOfDefinitions: Int = 4000)(implicit s: Semantics)
+  extends ForkJoinParallelCpgPass[Method](cpg) {
+
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  override def generateParts(): Array[Method] = cpg.method.toArray
+
+  override def runOnPart(builder: DiffGraphBuilder, method: Method): Unit = {
+    implicit val diffGraph: DiffGraphBuilder = builder
+    val problem = ReachingDefProblem.create(method)
+    if (shouldBailOut(method, problem)) {
+      logger.warn("Skipping.")
+    } else {
+      addEdgesToCapturedIdentifiersAndParameters(method)
+    }
+  }
+
+  private def addEdgesToCapturedIdentifiersAndParameters(method: Method)(implicit dstGraph: DiffGraphBuilder): Unit = {
+    val identifierDestPairs =
+      method._identifierViaContainsOut.flatMap { identifier =>
+        val firstAndLastUsageByMethod = identifierToFirstUsages(identifier).groupBy(_.method)
+        firstAndLastUsageByMethod.values
+          .filter(_.nonEmpty)
+          .map(x => (x.head, x.last))
+          .flatMap { case (firstUsage, lastUsage) =>
+            (identifier.lineNumber, firstUsage.lineNumber, lastUsage.lineNumber) match {
+              case (Some(iNo), Some(fNo), _) if iNo <= fNo => Some(identifier, firstUsage)
+              case (Some(iNo), _, Some(lNo)) if iNo >= lNo => Some(lastUsage, identifier)
+              case _ => None
+            }
+          }
+      }.distinct
+
+    identifierDestPairs
+      .foreach { case (src, dst) =>
+        addEdge(src, dst, nodeToEdgeLabel(src))
+      }
+    method.parameter.foreach { param =>
+      param.capturedByMethodRef.referencedMethod.ast.isIdentifier.foreach { identifier =>
+        addEdge(param, identifier, nodeToEdgeLabel(param))
+      }
+    }
+    val globalIdentifiers =
+      method.ast.isLiteral.flatMap(globalFromLiteral).collectAll[Identifier].l
+    globalIdentifiers
+      .foreach { global =>
+        identifierToFirstUsages(global).map { identifier =>
+          addEdge(global, identifier, nodeToEdgeLabel(global))
+        }
+      }
+  }
+
+  /** Before we start propagating definitions in the graph, which is the bulk of the work, we check how many definitions
+   * were are dealing with in total. If a threshold is reached, we bail out instead, leaving reaching definitions
+   * uncalculated for the method in question. Users can increase the threshold if desired.
+   */
+  private def shouldBailOut(method: Method, problem: DataFlowProblem[StoredNode, mutable.BitSet]): Boolean = {
+    val transferFunction = problem.transferFunction.asInstanceOf[ReachingDefTransferFunction]
+    // For each node, the `gen` map contains the list of definitions it generates
+    // We add up the sizes of these lists to obtain the total number of definitions
+    val numberOfDefinitions = transferFunction.gen.foldLeft(0)(_ + _._2.size)
+    logger.info("Number of definitions for {}: {}", method.fullName, numberOfDefinitions)
+    if (numberOfDefinitions > maxNumberOfDefinitions) {
+      logger.warn("{} has more than {} definitions", method.fullName, maxNumberOfDefinitions)
+      true
+    } else {
+      false
+    }
+  }
+
+  private def nodeToEdgeLabel(node: StoredNode): String = {
+    node match {
+      case n: MethodParameterIn => n.name
+      case n: CfgNode => n.code
+      case _ => ""
+    }
+  }
+
+  private def addEdge(fromNode: StoredNode, toNode: StoredNode, variable: String = "")(implicit
+                                                                                       dstGraph: DiffGraphBuilder
+  ): Unit = {
+    if (!fromNode.isInstanceOf[Unknown] && !toNode.isInstanceOf[Unknown]) {
+      (fromNode, toNode) match {
+        case (parentNode: CfgNode, childNode: CfgNode) if EdgeValidator.isValidEdge(childNode, parentNode) =>
+          dstGraph.addEdge(fromNode, toNode, EdgeTypes.REACHING_DEF, PropertyNames.VARIABLE, variable)
+        case _ =>
+
+      }
+    }
+  }
+
+}
