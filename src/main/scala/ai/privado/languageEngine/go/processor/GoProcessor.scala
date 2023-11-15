d656c28e5f3fd6241cb0b98e26cf0327997ff635
Ankit Kumar <ankit.kumar@privado.ai>
2023-11-15 12:19:50 +0530

    go exclusion fix

--- src/main/scala/ai/privado/languageEngine/go/processor/GoProcessor.scala
+++ src/main/scala/ai/privado/languageEngine/go/processor/GoProcessor.scala
@@ -209,7 +209,7 @@ object GoProcessor {
     // Converting path to absolute path, we may need that same as JS
     val absoluteSourceLocation = File(sourceRepoLocation).path.toAbsolutePath
     val cpgOutputPath          = s"$sourceRepoLocation/$outputDirectoryName/$cpgOutputFileName"
-    val excludeFileRegex       = ruleCache.getExclusionRegex
+    val excludeFileRegex       = ruleCache.getExclusionRegex.stripPrefix("(").stripSuffix(")")
 
     // Create the .privado folder if not present
     createCpgFolder(sourceRepoLocation);
