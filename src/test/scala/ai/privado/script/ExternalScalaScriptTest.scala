1bb5c5c27d8e1d1e5e59dea31aec5eb1cedb1cb2
Khemraj Rathore <khemraj.rathore@privado.ai>
2023-11-30 12:15:51 +0530

    formatting

--- src/test/scala/ai/privado/script/ExternalScalaScriptTest.scala
+++ src/test/scala/ai/privado/script/ExternalScalaScriptTest.scala
@@ -15,7 +15,7 @@ import org.scalatest.wordspec.AnyWordSpec
 import scala.collection.mutable
 class ExternalScalaScriptTest extends AnyWordSpec with Matchers with BeforeAndAfterAll {
   "External Scala script runner" should {
-    //TODO To fix the test case `fork := true` was added in build.sbt, which is causing failure in aws codebuild
+    // TODO To fix the test case `fork := true` was added in build.sbt, which is causing failure in aws codebuild
     "test sample external scala script" ignore {
       val scriptInstance = externalScript("""
                              |import io.shiftleft.codepropertygraph.Cpg
