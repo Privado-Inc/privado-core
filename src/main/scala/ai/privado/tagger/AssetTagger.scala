062473e20b819d10b08588d6c41b3b01985937cf
Khemraj Rathore <khemraj.rathore@privado.ai>
2024-02-04 19:00:17 +0530

    add - assetTagger

---
+++ src/main/scala/ai/privado/tagger/AssetTagger.scala
@@ -0,0 +1,32 @@
+package ai.privado.tagger
+
+import ai.privado.cache.RuleCache
+import ai.privado.model.{InternalTag, RuleInfo}
+import io.shiftleft.semanticcpg.language.*
+import io.shiftleft.codepropertygraph.generated.{Cpg, EdgeTypes, Languages, Operators}
+import ai.privado.utility.Utilities.storeForTag
+import io.shiftleft.codepropertygraph.generated.nodes.NewTag
+
+class AssetTagger(cpg: Cpg) extends PrivadoSimpleCpgPass(cpg) {
+
+  val sourceRegex = "(?i).*(mongo|sql|s3|oracle|dynamo|maria|hive|redis).*"
+  val valueRegex  = "(?i).*(http|mongo|sql|s3|oracle|dynamo|maria|hive|redis).*"
+  override def run(builder: DiffGraphBuilder): Unit = {
+    cpg.literal
+      .code(valueRegex)
+      .whereNot(_.code(".*[.](png|jpg|jpeg|jar|zip|xml|json|yml)$"))
+      .foreach(builder.addEdge(_, NewTag().name(InternalTag.PROBABLE_ASSET.toString), EdgeTypes.TAGGED_BY))
+
+    cpg.identifier
+      .name(sourceRegex)
+      .foreach(builder.addEdge(_, NewTag().name(InternalTag.PROBABLE_ASSET.toString), EdgeTypes.TAGGED_BY))
+
+    cpg.member
+      .name(sourceRegex)
+      .foreach(builder.addEdge(_, NewTag().name(InternalTag.PROBABLE_ASSET.toString), EdgeTypes.TAGGED_BY))
+
+    cpg.fieldAccess.fieldIdentifier
+      .canonicalName(sourceRegex)
+      .foreach(builder.addEdge(_, NewTag().name(InternalTag.PROBABLE_ASSET.toString), EdgeTypes.TAGGED_BY))
+  }
+}
