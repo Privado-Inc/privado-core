45c7c40473588e15480aee602597f3b300b160cb
Khemraj Rathore <khemraj.rathore@privado.ai>
2024-02-19 18:08:02 +0530

    improve regex

--- src/main/scala/ai/privado/exporter/ExporterUtility.scala
+++ src/main/scala/ai/privado/exporter/ExporterUtility.scala
@@ -404,16 +404,18 @@ object ExporterUtility {
       output.addOne("propertyNodesData" -> propertyNodesData.asJson)
 
       val probablePropertyNodes = propertyNodesData
+        .filterNot(_._3.matches(".*package.json"))
         .or(
           _.filter(
             _._1.matches(
-              "(?i).*(connection|host|database|location|uri|mongo|sql|postgres|s3|oracle|redis|bucket|dynamo|maria|db(_)?name).*"
+              "(?i).*(connection|host|database|location|uri$|mongo|sql|postgres|s3|oracle|redis|bucket|dynamo|maria|db(_)?name).*"
             )
           ),
           _.filter(_._2.matches("(?i).*(mongo|sql|postgres|aws|oracle|redis|dynamo|maria).*"))
         )
         .filterNot(_._2.matches(".*[.](png|jpg|jpeg|jar|zip|xml|json|yml)$"))
         .filterNot(_._2.matches("^(true|false)$"))
+        .filterNot(_._2.matches("regex[(]"))
         .l
 
       println("Printing probable assets")
