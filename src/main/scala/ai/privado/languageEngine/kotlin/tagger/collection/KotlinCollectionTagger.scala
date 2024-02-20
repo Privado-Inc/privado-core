69f964b6c986490f077c66e4a6742b9f5c7f9270
Sriram Viswanathan <sriram.viswanathan@privado.ai>
2024-02-20 11:23:53 +0530

    LE-1096 | Kotlin method full name tagger for collections
    Uses the Java collection tagger for deriving sources.

--- src/main/scala/ai/privado/languageEngine/kotlin/tagger/collection/KotlinCollectionTagger.scala
+++ src/main/scala/ai/privado/languageEngine/kotlin/tagger/collection/KotlinCollectionTagger.scala
@@ -11,20 +11,19 @@ import org.slf4j.LoggerFactory
 class KotlinCollectionTagger(cpg: Cpg, ruleCache: RuleCache) extends CollectionTagger(cpg, ruleCache) {
   private val logger = LoggerFactory.getLogger(this.getClass)
   override def generateParts(): Array[RuleInfo] =
+    // we want to look at methods, not annotations
     ruleCache.getRule.collections.filter(_.catLevelTwo == Constants.default).toArray
 
   override def runOnPart(builder: DiffGraphBuilder, ruleInfo: RuleInfo): Unit = {
     val (methodUrls, collectionMethodsCache) = collectUrlsFromMethodCalls(ruleInfo.combinedRulePattern)
     methodUrlMap.addAll(methodUrls)
+    // tag sources from Java collection tagger
     tagSources(builder, ruleInfo, collectionMethodsCache)
   }
 
   private def collectUrlsFromMethodCalls(combinedRulePatterns: String): (Map[Long, String], List[Method]) = {
-    val methodEndpoints = cpg.call.methodFullName(combinedRulePatterns)
-    (
-      methodEndpoints.map(ma => ma.method.method.id() -> ma.argument.isLiteral.head.code).toMap,
-      methodEndpoints.method.l
-    )
+    val methodEndpoints = cpg.call.methodFullName(combinedRulePatterns).l
+    (methodEndpoints.map(ma => ma.id() -> ma.argument.isLiteral.head.code).toMap, methodEndpoints.method.l)
   }
 
 }
