48b92c201d7eaa1996158561f4393049a71fc03f
Karan Batavia <karan.batavia@privado.ai>
2024-02-26 16:09:26 +0530

    tests for derived sources

--- src/test/scala/ai/privado/languageEngine/csharp/source/IdentifierTaggingTests.scala
+++ src/test/scala/ai/privado/languageEngine/csharp/source/IdentifierTaggingTests.scala
@@ -23,4 +23,35 @@ class IdentifierTaggingTests extends CSharpTestBase {
     }
   }
 
+  "Derived sources" should {
+    val (cpg, _) = code("""
+        |namespace Foo {
+        | public class Bar {
+        |   public int PhoneNumber {get; set;}
+        | }
+        | public class Baz {
+        |   public static void Main() {
+        |     Bar b = new Bar(1);
+        |   }
+        | }
+        |}
+        |""".stripMargin)
+
+    "tag the member inside a class" in {
+      cpg.member("PhoneNumber").tag.nameExact(Constants.id).value.l shouldBe List(
+        "Data.Sensitive.ContactData.PhoneNumber"
+      )
+    }
+
+    "be tagged as part of Identifier tagger" in {
+      val barId = cpg.identifier("b").l
+      barId.tag
+        .where(_.nameExact(InternalTag.OBJECT_OF_SENSITIVE_CLASS_BY_MEMBER_NAME.toString))
+        .value
+        .head shouldBe "Data.Sensitive.ContactData.PhoneNumber"
+      barId.tag.where(_.nameExact(Constants.id)).size shouldBe 1
+      barId.tag.where(_.nameExact(Constants.catLevelOne)).value.head shouldBe "DerivedSources"
+    }
+  }
+
 }
