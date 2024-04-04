c9f255d9be4cc97a5f3df0f462a1c58566f0ec68
Pandurang Patil <5101898+pandurangpatil@users.noreply.github.com>
2024-04-04 20:03:43 +0530

    Refactored base Test fixtures
    Refactored base implementation of test fixtures and made it more simpler
    to write the code in test case itself. Also in earlier case for each
    `in` clause will execute the scan every time for sub sections of the
    unit test. With the updated implementation, now the scan will be invoked
    only once for each `should` section.

--- src/test/scala/ai/privado/languageEngine/java/DummyJavaTest.scala
+++ src/test/scala/ai/privado/languageEngine/java/DummyJavaTest.scala
@@ -1,33 +1,36 @@
 package ai.privado.languageEngine.java
 
 import ai.privado.model.Constants
+import ai.privado.testfixtures.JavaFrontendTestSuite
 import io.shiftleft.semanticcpg.language.*
 import io.circe.Json
 import io.circe.syntax.EncoderOps
 
-class DummyJavaTest extends JavaTestCpgBase {
+class DummyJavaTest extends JavaFrontendTestSuite {
 
   "Dummy test to write test case" should {
+    val cpg = code("""
+        |public class Dummy {
+        |    private String firstName;
+        |    public Dummy(String firstName) {
+        |        this.firstName = firstName;
+        |    }
+        |    public String getFirstName() {
+        |        return firstName;
+        |    }
+        |    public void setFirstName(String firstName) {
+        |        this.firstName = firstName;
+        |    }
+        |}
+        |""".stripMargin).generateScanResult()
 
-    val sourceCodeLocation = "src/test/resources/java/dummy"
     "show using only cpg" in {
-      val cpg = withCpg(sourceCodeLocation)
-
       cpg.identifier("firstName").nonEmpty shouldBe true
     }
 
     "show using output json" in {
-      val outputJson = withJson(sourceCodeLocation)
-
-      outputJson(Constants.repoName) shouldBe "dummy".asJson
-    }
-
-    "show using cpg with output json" in {
-      val (cpg, outputJson) = withCpgAndJson(sourceCodeLocation)
-
-      cpg.identifier("firstName").nonEmpty shouldBe true
-      outputJson(Constants.repoName) shouldBe "dummy".asJson
+      val outputJson = cpg.getPrivadoJson()
+      outputJson(Constants.repoName).asString.get should startWith("x2cpgTestTmpDir")
     }
   }
-
 }
