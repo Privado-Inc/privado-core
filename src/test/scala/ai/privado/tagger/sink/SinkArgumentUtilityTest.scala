3a8c0a22475a330e9b5003a82441b4d9b41eb058
Dattaprasad Mundada <dattaprasadmundada@Dattaprasads-MacBook-Pro.local>
2024-04-23 07:48:38 +0530

    Add more negative tests

--- src/test/scala/ai/privado/tagger/sink/SinkArgumentUtilityTest.scala
+++ src/test/scala/ai/privado/tagger/sink/SinkArgumentUtilityTest.scala
@@ -53,6 +53,10 @@ class SinkArgumentUtilityTest extends AnyFlatSpec with Matchers {
     deserializedMap shouldBe empty
   }
 
+  /** Below test-case covers below conditions:
+    *   1. Simple literal arguments 2. Nested arguments 3. Payload Method 4. lodash pick method 5. Block Node (part of
+    *      nested structure) 6. Spread Node
+    */
   "For googleTagManagerPixelRuleId verify the arguments list" should "tag the call node as sink by matching rule with cpg.code" in {
     val cpg = code("""
         |import { pick } from 'lodash';
@@ -121,6 +125,28 @@ class SinkArgumentUtilityTest extends AnyFlatSpec with Matchers {
       val argumentsListValue = argumentsList.getOrElse(key, "")
       assert(argumentsListValue == value, s"Expected value '$value' for key '$key', but got '$argumentsListValue'")
     }
+  }
+
+  "For googleTagManagerPixelRuleId verify the arguments list" should "check for no arguments passed" in {
+    val cpg = code("""
+        |globalThis.dataLayer.push();
+        |""".stripMargin)
+    val gaSinkNode = cpg.call.name("push").head
+    gaSinkNode.tag.nameExact(Constants.id).value.headOption shouldBe Some(Constants.googleTagManagerPixelRuleId)
+    val encodedArgumentsList = gaSinkNode.tag.nameExact(Constants.arguments).value.headOption.getOrElse("{}")
+    val argumentsList        = SinkArgumentUtility.deserializedArgumentString(encodedArgumentsList)
+    argumentsList.isEmpty shouldBe true
+  }
+
+  "For other sinks verify" should "arguments tag should not be added" in {
+    val cpg = code("""
+        |const products = { "a": "a1", "b": "b1"  };
+        |pick(products, ["a", "b"]);
+        |""".stripMargin)
+    val gaSinkNode = cpg.call.name("pick").head
+    val encodedArgumentsList =
+      gaSinkNode.tag.nameExact(Constants.arguments).value.headOption.getOrElse("{ \"empty\": true }")
+    encodedArgumentsList shouldBe "{ \"empty\": true }"
   }
 }
 
