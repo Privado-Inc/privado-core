57f9df2dc7a6f74036ab0a04a5dd896cca058c99
KhemrajSingh Rathore <khemraj.rathore@privado.ai>
2024-05-08 13:15:52 +0530

    fix - tagging in sql query tagger to split and match (#1109)
    * fix - tagging in sql query tagger to split and match
    
    * remove unwanted code

---
+++ src/test/scala/ai/privado/tagger/source/SqlQueryTaggerTests.scala
@@ -0,0 +1,24 @@
+package ai.privado.tagger.source
+
+import ai.privado.model.Constants
+import ai.privado.testfixtures.DefaultFrontendTestSuite
+import ai.privado.semantic.Language.*
+import io.shiftleft.semanticcpg.language.*
+
+class SqlQueryTaggerTests extends DefaultFrontendTestSuite {
+
+  "Sql Query tagger" should {
+    val cpg = code(
+      """
+                     |select DISTINCT(firstName) As firstName,
+                     |lastName from customer;
+                     |""".stripMargin,
+      "sample1.sql"
+    )
+
+    "tag nodes when source present in space split" in {
+      val List(firstName, lastName) = cpg.sqlColumn.l
+      firstName.tag.nameExact(Constants.id).value.l shouldBe List("Data.Sensitive.FirstName")
+    }
+  }
+}
