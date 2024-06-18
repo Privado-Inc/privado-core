8aeb5ec678d96e6a5d730559a3cb0a5740b26ea0
Ankit Kumar <ankit.kumar@privado.ai>
2024-06-18 14:18:41 +0530

    removed unnecessary code

--- src/test/scala/ai/privado/exporter/JavaScriptLanguageEgressTest.scala
+++ src/test/scala/ai/privado/exporter/JavaScriptLanguageEgressTest.scala
@@ -70,44 +70,4 @@ class JavaScriptLanguageEgressTest extends JavaScriptFrontendTestSuite {
       )
     }
   }
-
 }
-
-//class JavaScriptLanguageEgressTest extends JavascriptTaggingTestBase {
-//
-//  override val rule: ConfigAndRules            = ConfigAndRules()
-//  override val packageJsonFileContents: String = ""
-//  val appCache                                 = new AppCache()
-//
-//  override def beforeAll(): Unit = {
-//    super.beforeAll()
-//    appCache.repoLanguage = Language.JAVA
-//  }
-//
-//  override val javascriptFileContents: String =
-//    """
-//      | import { requests } from "service/settings"
-//      | const endpoint = { getUserDetails: (id) => `v1/api/user${id}`, getLogin: "v1/api/login" }
-//      | const signup = "v1/api" + "/signup"
-//      | const tag = "<div>something else <h1>heelo</h1></div>"
-//      | const newEndpoint = "api/v1/" + "user/meta" + "/profile"
-//      | const signup = requests("v1/api/users/meta")
-//      |""".stripMargin
-//
-//  "Javascript code egresses" should {
-//    "collect egress url for javascript code" in {
-//      val httpConnectionExporter    = new HttpConnectionMetadataExporter(cpg, ruleCache, appCache)
-//      val egressesFromLanguageFiles = httpConnectionExporter.getEgressUrlsFromCodeFiles
-//      egressesFromLanguageFiles.size shouldBe 6
-//      egressesFromLanguageFiles shouldBe List(
-//        "v1/api/userid",
-//        "v1/api/login",
-//        "v1/api/signup",
-//        "api/v1/user/meta",
-//        "api/v1/ + user/meta/profile",
-//        "v1/api/users/meta"
-//      )
-//    }
-//  }
-//
-//}
