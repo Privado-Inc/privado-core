724a68fa2aa521656bdf602b0ce8e3883a24f678
Dattaprasad Mundada <dattaprasadmundada@Dattaprasads-MacBook-Pro.local>
2024-06-19 08:13:03 +0530

    Add filters for python #2

--- src/main/scala/ai/privado/audit/DEDSourceDiscovery.scala
+++ src/main/scala/ai/privado/audit/DEDSourceDiscovery.scala
@@ -26,24 +26,24 @@ object DEDSourceDiscoveryUtils {
 
   // Regular expression pattern to filter common language types
   private val filterCommonLangTypes =
-    "(?i)(class|window|str|list|dict|bool|chr|icon|boolean|number|bigdecimal|jwt|let|define|enum|asttype|nil|null|none|not|uuid|java|undefined|nan|empty|objectid|_id|tostring|valueof|json|mime|true|false|before|after|arr|typeof|match|to_dict|toarray|todate|case|<global>|<fakeNew>|(buf|array|int|num|type|float|byte|string|blob).{0,1}|set|map|hashmap|vector|deque|function|method|property|char|short|long|double|decimal|datetime|date|time|timestamp|enum|flag|void|interface|trait|package|namespace|record|tuple|struct|component|hook|hoc|service)"
+    "(?i)(class|window|str|list|dict|bool|chr|icon|boolean|number|bigdecimal|jwt|let|define|enum|asttype|nil|null|none|not|uuid|java|undefined|nan|empty|objectid|_id|tostring|valueof|json|mime(type){0,1}|true|false|before|after|arr|typeof|match|to_dict|toarray|todate|case|<global>|<fakeNew>|(buf|array|int|num|type|float|byte|string|blob|object).{0,1}|set|map|hashmap|vector|deque|function|method|property|char|short|long|double|decimal|datetime|date|time|timestamp|enum|flag|void|interface|trait|package|namespace|record|tuple|struct|component|hook|hoc|service)"
 
   // Regular expression pattern to filter common variable names
   private val filterCommonVars =
-    "(?i)(cls|self|ctx|constructor|context|super|new|prototype|main|use|foreach|copy|skip|replace|.*(dto|exception)|slice|fill|some|every|concat|contains|apply|merge|stmt|format|name|data|regexp|.{0,1}(sort|in|slug|match|ne|regex|or|sum|and)|session|status|event|env|cmd|push|join|split|splice|filter|reduce|shift|unshift|retry|start|buffer|thread|length|staticmethod|app|next|end|req|console|push|pop|handler|server|catch|then|uri|split|exp|other|info|debug|warning|critical|exception|size|max|text|http|query|href|write|(sql|row|len|err|res|ret|obj|msg|val|key|item|url|tmp|col|file|img|test|result|path|module|import|export|log|key|value|include|load|dump).{0,1})"
+    "(?i)(.*(\\\\|\\/).*|cls|self|ctx|constructor|context|super|new|prototype|main|use|foreach|copy|trim|skip|replace|.*(dto|exception)|slice|fill|some|every|concat|contains|apply|merge|stmt|format|where|name|data|regexp|.{0,1}(sort|in|slug|match|ne|regex|or|sum|and)|session|status|patch|accept|event|env|cmd|push|join|split|splice|filter|reduce|shift|unshift|retry|start|buffer|thread|length|staticmethod|app|next|end|req|console|push|pop|handler|server|catch|then|uri|split|exp|other|info|debug|warning|critical|exception|size|max|text|http|query|href|write|(sql|row|len|err|res|ret|obj|msg|val|key|item|url|tmp|col|file|img|test|result|path|module|import|export|log|key|value|include|load|dump).{0,1})"
 
   // List of prefixes to filter out common variables that start with these values
   private val filterCommonVarsStartsWith =
-    "$obj|$group|$set|$id|$lte|$gte|$options|__|_tmp_|tmp|$iterLocal|_result|file|return|is|sha_|this|get|set|post|put|update|create|clear|check|find|insert|assert|parse|generate|validate|process|download|upload|delete|handle|mongo|validation|exception|param|attr|arg|_iterator|{|log|error|iterator|logger|<tmp-|iter_tmp|toLocale|indexOf"
+    "$obj|$group|$set|$id|$lte|$gte|$options|__|_tmp_|tmp|$iterLocal|_result|file|return|is|sha_|this|get|set|post|put|update|create|clear|check|find|insert|execute|aggregate|assert|parse|generate|validate|process|download|upload|delete|handle|mongo|http|validation|exception|param|attr|arg|_iterator|{|[|log|error|iterator|logger|<tmp-|iter_tmp|toLocale|indexOf|page"
   private val filterCommonVarsStartsWithArr = filterCommonVarsStartsWith.split("\\|")
 
   // Additional Language specific filters
   def getLanguageSpecificFilters(lang: Language): String = {
     lang match {
       case Language.JAVASCRIPT =>
-        "(?i)(<anon-class>.*|i18n|ajv|axios|require|express|moment|document|ref|hasOwnProperty|date|usestate|useeffect|dispatch|ngif|ngforof|inject|component|router|validators|formgroup|formcontrol|subscribe|observable|jquery|vue|react|angular|.*[.]js)"
+        "(?i)((<anon-class>|<lambda>|_computed_object_property_)\\d+|promise|i18n|ajv|async|axios|connectdb|require.{0,1}|express|moment|document|css|children|style|ref|props|success|route|state|.*reducer.{0,1}|action|link|render|onclick|hasOwnProperty|stringify|date|use(state|effect|dispatch|form|selector|query|memo|router|swrconfig)|request|send|form|loading|dispatch|ngif|ngforof|inject|component|router|validators|formgroup|formcontrol|subscribe|observable|jquery|vue|react|angular|.*[.](js|css|ts))"
       case Language.PYTHON =>
-        "(?i)(<module>|.*<meta>|print|boto.{0,1}|s3|requests|append|extend|list_objects.{0,6}|pytest|datetime|pandas|numpy|scipy|sklearn|matplotlib|flask|django|argparse|os|sys)"
+        "(?i)(<module>|.*<meta>|.*_tmp\\d+|print|boto.{0,1}|s3|request.{0,1}|classmethod|cursor|append|now|acknowledged|apply_async|limit|statusCode|extend|list_objects.{0,6}|pytest|datetime|pandas|numpy|scipy|sklearn|matplotlib|flask|django|argparse|os|sys|format_exc|read|std(out|in|err)|exit|compile|open)"
       case Language.JAVA | Language.KOTLIN =>
         "(?i)(system|out|buf|(com|io|org|net|androidx|software|io)[.]{1}.*|printstream|scanner|stringbuilder|thread|exception|bufferedreader|inputstream|outputstream|integer|character)"
       case Language.GO =>
@@ -51,7 +51,7 @@ object DEDSourceDiscoveryUtils {
       case Language.PHP =>
         "(?i)(_post|_get|_session|_files|_server|_cookie|uid|mysqli|pdo|print_r|var_dump|echo|header|session_start|session_destroy)"
       case Language.RUBY =>
-        "(?i)(node|tree|raise|object|gem|rails|sinatra|rake|erb|haml)"
+        "(?i)(node|tree|raise|gem|rails|sinatra|rake|erb|haml)"
       case Language.CSHARP =>
         "(?i)(table|migrationBuilder|modelBuilder|program|yaml|context|cts|instance|idx|system|console|io|collections|generic|threading|tasks|linq|xml|entity|configuration|component|service)"
     }
@@ -123,6 +123,8 @@ object DEDSourceDiscoveryUtils {
                 .filter(i => !i.name.matches(filterCommonVars))
                 // Filter out members matching language-specific filters
                 .filter(i => !i.name.matches(DEDSourceDiscoveryUtils.getLanguageSpecificFilters(lang)))
+                // Filter out identifier which are potentially function calls
+                .filter(i => !i.typeFullName.matches("\\(.*\\).=>.*"))
                 .dedup
                 .l
             memberInfoMap.put(typeDeclNode, filteredMembers)
@@ -204,6 +206,8 @@ object DEDSourceDiscoveryUtils {
                 .filter(i => !i.name.matches(filterCommonVars))
                 // Filter out members matching language-specific filters
                 .filter(i => !i.name.matches(DEDSourceDiscoveryUtils.getLanguageSpecificFilters(lang)))
+                // Filter out identifier which are potentially function calls
+                .filter(i => !i.typeFullName.matches("\\(.*\\).=>.*"))
                 .dedup
                 .l
             parameters ++= filteredMembers
@@ -275,7 +279,9 @@ object DEDSourceDiscoveryUtils {
           // Filter out identifiers matching language-specific filters
           .filter(i => !i.name.matches(DEDSourceDiscoveryUtils.getLanguageSpecificFilters(lang)))
           // Filter out identifier which are potentially function calls
-          .filter(i => !i.typeFullName.matches("\\(.*\\).=>.*"))
+          .filter(i => !i.typeFullName.matches("\\(.*\\).=>.*|(\\w|_|-)+:(\\w|_|-)+"))
+          // Filter out identifier which are potentially import calls
+          .filter(i => !i.astSiblings.isCall.name("import|require").nonEmpty)
           .l
       }
       case Failure(ex) => {
@@ -343,6 +349,8 @@ object DEDSourceDiscoveryUtils {
           .filter(i => !i.canonicalName.matches(filterCommonVars))
           // Filter out identifiers matching language-specific filters
           .filter(i => !i.canonicalName.matches(DEDSourceDiscoveryUtils.getLanguageSpecificFilters(lang)))
+          // Filter out identifier which are potentially export calls
+          .filter(i => !i.astSiblings.isIdentifier.name("import|exports").nonEmpty)
           // Filter out fieldIdentifiers like `session` of format `res.session.XXX`
           .whereNot(_.astParent.astParent.isCall.name("<operator>.fieldAccess"))
           .map((i: FieldIdentifier) => {
@@ -394,56 +402,6 @@ object DEDSourceDiscoveryUtils {
     })
 
     workbookResult
-  }
-
-  // Add Members in workbookResult for given className set
-  def getMemberUsingClassName(
-    xtocpg: Try[Cpg],
-    classNameSet: Set[String],
-    lang: Language
-  ): Map[TypeDecl, List[Member]] = {
-    logger.info("Process Member Name from cpg")
-    val memberInfoMap = mutable.HashMap[TypeDecl, List[Member]]()
-
-    xtocpg match {
-      case Success(cpg) => {
-        classNameSet.foreach(className => {
-          try {
-            cpg.typeDecl
-              .where(_.fullName(className))
-              .foreach(typeDeclNode => {
-                if (typeDeclNode.member.nonEmpty) {
-                  val members =
-                    typeDeclNode.member
-                      // Filter out members with length <= 2
-                      .filter(i => i.name.length > 2)
-                      // Filter out members that start with any of the specified prefixes
-                      .filter(i =>
-                        !filterCommonVarsStartsWithArr.exists(xx => i.name.toLowerCase.startsWith(xx.toLowerCase))
-                      )
-                      // Filter out members matching common language types pattern
-                      .filter(i => !i.name.matches(filterCommonLangTypes))
-                      // Filter out members matching common variable names pattern
-                      .filter(i => !i.name.matches(filterCommonVars))
-                      // Filter out members matching language-specific filters
-                      .filter(i => !i.name.matches(DEDSourceDiscoveryUtils.getLanguageSpecificFilters(lang)))
-                      .dedup
-                      .l
-                  memberInfoMap.put(typeDeclNode, members)
-                }
-              })
-          } catch {
-            case ex: Exception =>
-              logger.debug(f"Skipping the class ${className} due to invalid regex issue", ex)
-          }
-        })
-      }
-      case Failure(exception) => {
-        logger.debug("Failed to process member name from cpg", exception)
-      }
-    }
-    logger.info("Successfully Processed member name from cpg")
-    memberInfoMap.toMap
   }
 
   def getHeaderList(): List[String] = {
@@ -570,7 +528,7 @@ object DEDSourceDiscovery {
           eliminateEmptyCellValueIfExist(item(0)),
           eliminateEmptyCellValueIfExist(item(1)),
           eliminateEmptyCellValueIfExist(item(3)),
-          eliminateEmptyCellValueIfExist(item(4)),
+          eliminateEmptyCellValueIfExist(if (item(4).startsWith("{")) "NA" else item(4)),
           item(5) == "true",
           eliminateEmptyCellValueIfExist(item(6)),
 
