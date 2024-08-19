dcd4b3d3ed00e3698c0dfc2d0f786989102dbb0b
Karan Batavia <118820668+karan-batavia@users.noreply.github.com>
2024-08-19 10:57:46 +0530

    Add logs and handle exceptions for `InferenceApiEndpointTagger` (#1249)
    add logs and handle exceptions for inference api endpoint tagger

--- src/main/scala/ai/privado/tagger/sink/api/InferenceAPIEndpointTagger.scala
+++ src/main/scala/ai/privado/tagger/sink/api/InferenceAPIEndpointTagger.scala
@@ -12,6 +12,8 @@ import io.shiftleft.codepropertygraph.generated.nodes.AstNode
 import io.shiftleft.semanticcpg.language.*
 import org.slf4j.LoggerFactory
 
+import scala.util.{Try, Success, Failure}
+
 /** Read the inference rule defined in passed ruleCache, and tag the API Sink and the corresponding API Endpoint
   * @param cpg
   * @param ruleCache
@@ -21,27 +23,23 @@ class InferenceAPIEndpointTagger(cpg: Cpg, ruleCache: RuleCache) extends Privado
   private val logger = LoggerFactory.getLogger(getClass)
 
   override def generateParts(): Array[RuleInfo] =
-    ruleCache.getRule.inferences.filter(_.catLevelTwo.equals(Constants.apiEndpoint)).toArray
+    Try(ruleCache.getRule.inferences.filter(_.catLevelTwo.equals(Constants.apiEndpoint)).toArray) match
+      case Success(rules) => rules
+      case Failure(exception: Exception) => {
+        logger.error("Cannot generateParts for InferenceAPIEndpointTagger: ", exception.getMessage)
+        Array.empty[RuleInfo]
+      }
+      case Failure(_) => {
+        logger.error("Cannot generateParts for InferenceAPIEndpointTagger")
+        Array.empty[RuleInfo]
+      }
 
   override def runOnPart(builder: DiffGraphBuilder, ruleInfo: RuleInfo): Unit = {
+    try {
+      val domain = ruleInfo.domains.headOption.getOrElse("")
 
-    val domain = ruleInfo.domains.headOption.getOrElse("")
-
-    ruleInfo.filterProperty match {
-      case METHOD_FULL_NAME_WITH_LITERAL =>
-        val apiSinks = cpg.call
-          .or(
-            _.methodFullName(ruleInfo.combinedRulePattern),
-            _.filter(_.dynamicTypeHintFullName.exists(_.matches(ruleInfo.combinedRulePattern)))
-          )
-          .l
-
-        apiSinks.foreach { apiCall => tagNode(builder, apiCall, domain) }
-
-      case METHOD_FULL_NAME_WITH_PROPERTY_NAME =>
-        val apiUrlFromProperty = cpg.property.name(domain).value.headOption
-
-        if (apiUrlFromProperty.isDefined) {
+      ruleInfo.filterProperty match {
+        case METHOD_FULL_NAME_WITH_LITERAL =>
           val apiSinks = cpg.call
             .or(
               _.methodFullName(ruleInfo.combinedRulePattern),
@@ -49,10 +47,27 @@ class InferenceAPIEndpointTagger(cpg: Cpg, ruleCache: RuleCache) extends Privado
             )
             .l
 
-          apiSinks.foreach { apiCall => tagNode(builder, apiCall, apiUrlFromProperty.get) }
-        }
+          apiSinks.foreach { apiCall => tagNode(builder, apiCall, domain) }
 
-      case _ =>
+        case METHOD_FULL_NAME_WITH_PROPERTY_NAME =>
+          val apiUrlFromProperty = cpg.property.name(domain).value.headOption
+
+          if (apiUrlFromProperty.isDefined) {
+            val apiSinks = cpg.call
+              .or(
+                _.methodFullName(ruleInfo.combinedRulePattern),
+                _.filter(_.dynamicTypeHintFullName.exists(_.matches(ruleInfo.combinedRulePattern)))
+              )
+              .l
+
+            apiSinks.foreach { apiCall => tagNode(builder, apiCall, apiUrlFromProperty.get) }
+          }
+
+        case _ =>
+      }
+    } catch {
+      case ex => logger.error("Error while executing InferenceApiEndpointTagger runOnPart: ", ex.getMessage)
+      case _  => logger.error("Cannot runOnPart for InferenceApiEndpointTagger")
     }
   }
 
