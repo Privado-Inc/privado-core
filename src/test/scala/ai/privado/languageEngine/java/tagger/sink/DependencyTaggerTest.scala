bda64eec8e225a265fdf31621b39673f5327a3f2
Pandurang Patil <5101898+pandurangpatil@users.noreply.github.com>
2024-08-22 15:05:46 +0530

    Additional unit tests with multiple sinks

--- src/test/scala/ai/privado/languageEngine/java/tagger/sink/DependencyTaggerTest.scala
+++ src/test/scala/ai/privado/languageEngine/java/tagger/sink/DependencyTaggerTest.scala
@@ -8,6 +8,8 @@ import ai.privado.model.*
 import ai.privado.model.exporter.{DataFlowSubCategoryPathExcerptModel, SinkProcessingModel}
 import ai.privado.testfixtures.JavaFrontendTestSuite
 
+import scala.collection.mutable
+
 class DependencyTaggerTest extends JavaFrontendTestSuite with SinkExporterValidator {
 
   "Java pom.xml simple use case" should {
@@ -28,8 +30,26 @@ class DependencyTaggerTest extends JavaFrontendTestSuite with SinkExporterValida
         "",
         Language.JAVA,
         Array()
+      ),
+      RuleInfo(
+        "ThirdParties.SDK.Google.Sheets",
+        "Google Sheets",
+        "Third Parties",
+        FilterProperty.METHOD_FULL_NAME,
+        Array("sheets.google.com"),
+        List(".*(com.google.apis).*"),
+        false,
+        "",
+        Map(),
+        NodeType.REGULAR,
+        "",
+        CatLevelOne.SINKS,
+        "",
+        Language.JAVA,
+        Array()
       )
     )
+
     val configAndRule = ConfigAndRules(sinks = dynamicRule)
     val ruleCache     = RuleCache().setRule(configAndRule)
     val twilioDep = List(
@@ -43,6 +63,18 @@ class DependencyTaggerTest extends JavaFrontendTestSuite with SinkExporterValida
         List("www.twilio.com"),
         List(),
         32,
+        "pom.xml"
+      ),
+      DependencyInfo(
+        "com.google.apis",
+        "google-api-services-sheets",
+        "v4-rev493-1.23.0",
+        "<artifactId>google-api-services-sheets</artifactId>",
+        "ThirdParties.SDK.Google.Sheets",
+        "Google Sheets",
+        List("sheets.google.com"),
+        List(),
+        42,
         "pom.xml"
       )
     )
@@ -86,6 +118,11 @@ class DependencyTaggerTest extends JavaFrontendTestSuite with SinkExporterValida
         |			<artifactId>spring-boot-starter-test</artifactId>
         |			<scope>test</scope>
         |		</dependency>
+        |  		<dependency>
+        |			<groupId>com.google.apis</groupId>
+        |			<artifactId>google-api-services-sheets</artifactId>
+        |			<version>v4-rev493-1.23.0</version>
+        |		</dependency>
         |	</dependencies>
         |	<build>
         |		<plugins>
@@ -100,9 +137,17 @@ class DependencyTaggerTest extends JavaFrontendTestSuite with SinkExporterValida
       "pom.xml"
     ).withDependencies(twilioDep).withRuleCache(ruleCache)
 
-    "Twilio dependency should get added in processing section" in {
-      val sinkProcessing = getSinkProcessings(cpg.getPrivadoJson())
-      sinkProcessing shouldBe List(
+    "3p dependency should get added in processing section" in {
+      val outjson                                                = cpg.getPrivadoJson()
+      val sinkProcessing                                         = getSinkProcessings(outjson)
+      val sinkProceMap: mutable.Map[String, SinkProcessingModel] = mutable.Map[String, SinkProcessingModel]()
+      sinkProcessing.size shouldBe 2
+      sinkProcessing.foreach(sink => {
+        sinkProceMap.put(sink.sinkId, sink)
+      })
+
+      val twilioSink = sinkProceMap.get("ThirdParties.SDK.twilio")
+      twilioSink shouldBe Some(
         SinkProcessingModel(
           sinkId = "ThirdParties.SDK.twilio",
           occurrences = List(
@@ -113,6 +158,24 @@ class DependencyTaggerTest extends JavaFrontendTestSuite with SinkExporterValida
               fileName = "pom.xml",
               excerpt =
                 "\t\t\t<groupId>org.springframework.boot</groupId>\n\t\t\t<artifactId>spring-boot-starter-web</artifactId>\n\t\t</dependency>\n\t\t<dependency>\n\t\t\t<groupId>com.twilio.sdk</groupId>\n\t\t\t<artifactId>twilio</artifactId> /* <===  */ \n\t\t\t<version>8.19.1</version>\n\t\t</dependency>\n\t\t<dependency>\n\t\t\t<groupId>org.springframework.boot</groupId>\n\t\t\t<artifactId>spring-boot-starter-test</artifactId>",
+              arguments = None
+            )
+          )
+        )
+      )
+
+      val gsheetSink = sinkProceMap.get("ThirdParties.SDK.Google.Sheets")
+      gsheetSink shouldBe Some(
+        SinkProcessingModel(
+          sinkId = "ThirdParties.SDK.Google.Sheets",
+          occurrences = List(
+            DataFlowSubCategoryPathExcerptModel(
+              sample = "<artifactId>google-api-services-sheets</artifactId>",
+              lineNumber = 42,
+              columnNumber = -1,
+              fileName = "pom.xml",
+              excerpt =
+                "\t\t\t<artifactId>spring-boot-starter-test</artifactId>\n\t\t\t<scope>test</scope>\n\t\t</dependency>\n  \t\t<dependency>\n\t\t\t<groupId>com.google.apis</groupId>\n\t\t\t<artifactId>google-api-services-sheets</artifactId> /* <===  */ \n\t\t\t<version>v4-rev493-1.23.0</version>\n\t\t</dependency>\n\t</dependencies>\n\t<build>\n\t\t<plugins>",
               arguments = None
             )
           )
